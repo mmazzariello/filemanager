843fceac7b2686eb2f6db4f9cd0f89124d1f4a0e
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-03-10 10:57:08 -0300

    ADD styles in file content component - desktop breakpoint

--- frontend/src/components/FileContent.jsx
+++ frontend/src/components/FileContent.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 import styles from "./FileContent.module.css";
 
 function FileContent({ file = null }) {
   return (
     <div className={styles.FileContent}>
-      <pre>
-        {file?.content && atob(file.content)}
-      </pre>
+      <pre>{file?.content && atob(file.content)}</pre>
     </div>
   );
 }
 
 FileContent.propTypes = {
-  file: PropTypes.object
+  file: PropTypes.object,
 };
 
 export default FileContent;
